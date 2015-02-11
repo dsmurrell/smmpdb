679a79b3786fc9fd7d9860471e436db9b15bdb15
Daniel Murrell <dsmurrell@gmail.com>
2015-02-11 09:44:05 +0000

    test

--- fabfile.py
+++ fabfile.py
@@ -14,8 +14,8 @@ def deploy():
     with cd('~/smmpdb'):
         run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && git pull https://github.com/dsmurrell/smmpdb.git')
         run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && sudo pip install -r requirements.txt')
-        run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && rabbitmq-server &')
-        run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && python manage.py migrate repo')
+        #run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && rabbitmq-server &')
+        run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && python manage.py migrate')
         #run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && python manage.py test repo')
         run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && sudo /etc/init.d/apache2 restart')
         
