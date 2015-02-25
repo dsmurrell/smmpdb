5bbb6748c1cd92f82645411209f178e998240f95
Daniel Murrell <dsmurrell@gmail.com>
2015-02-25 13:23:31 +0000

    trying again

--- fabfile.py
+++ fabfile.py
@@ -16,8 +16,8 @@ def deploy():
         run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && git pull https://github.com/dsmurrell/smmpdb.git')
         run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && sudo pip install -r requirements.txt')
         run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && python manage.py migrate')
-        sudo('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && pkill celery')
-        sudo('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && celery -A smmpdb worker -l info &')
+        sudo('pkill celery')
+        sudo('celery -A smmpdb worker -l info &')
         run('source /home/dsm38/.virtualenvs/smmpdb/bin/activate && sudo /etc/init.d/apache2 restart')
         
 def both():
